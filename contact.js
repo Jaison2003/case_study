bf7ee3884604400f018a73e2a6cd2136e7d6185f
Jaison2003 <jaisonsamjiju93@gmail.com>
2022-06-25 14:02:11 +0530

    message

---
+++ contact.js
@@ -0,0 +1,14 @@
+readBtnEvent=()=>{
+    var name=document.getElementById("name").value
+    var message=document.getElementById("message").value
+    var phone=document.getElementById("number").value
+
+    console.log(name)
+    alert(name)
+
+    console.log(message)
+    alert(message)
+
+    console.log(number)
+    alert(Number)
+}
